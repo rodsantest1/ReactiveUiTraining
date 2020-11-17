b2609182873c5dee41e4cfc9caee29fc86925b70
Rodney Santiago <GD000007711@ups.com>
2020-11-17 13:48:26 -0500

    Add project files.

---
+++ ClassLibrary1/MainViewModel.cs
@@ -0,0 +1,57 @@
+﻿using ClassLibrary1.Models;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Text;
+
+namespace ClassLibrary1.ViewModels
+{
+    public class MainViewModel : ReactiveObject
+    {
+        //Reactive properties
+        [Reactive] public string SpinnerVisibility { get; set; } = "Hidden";
+
+        //Button interactions - initializing and cannot change unless through observable
+        private readonly Interaction<MainModel, string> _buttonInteraction;
+
+        public Interaction<MainModel, string> ButtonInteraction => this._buttonInteraction;
+
+        //Reactive commands - initialize and set
+        public ReactiveCommand<MainModel, string> FireButtonCommand { get; set; }
+
+        public MainViewModel()
+        {
+            InitObservables();
+
+            _buttonInteraction = new Interaction<MainModel, string>();
+
+            FireButtonCommand = ReactiveCommand.CreateFromObservable<MainModel, string>(this.FireButtonClickHandler);
+        }
+
+        private IObservable<string> FireButtonClickHandler(MainModel arg)
+        {
+            arg = new MainModel();
+            arg.Name = "Content was saved.";
+
+            return Observable.StartAsync(async () =>
+            {
+                await System.Threading.Tasks.Task.Delay(3000); //Simulate work
+
+                return await this._buttonInteraction.Handle(arg);
+            });
+        }
+
+        private void InitObservables()
+        {
+            //Creates an observable that monitors the status of the button
+            //and then updates the spinner based on the status of the button
+            this.WhenAnyObservable(x => x.FireButtonCommand.IsExecuting)
+                .Select(x => x == true ? this.SpinnerVisibility = "Visible" : this.SpinnerVisibility = "Hidden")
+                .ObserveOn(RxApp.MainThreadScheduler)
+                //.Delay(TimeSpan.FromSeconds(3))
+                .Subscribe();
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using ClassLibrary1.ViewModels;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ReactiveWindow<MainViewModel>
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ViewModel = new MainViewModel();
+
+            this.WhenActivated(disposables =>
+            {
+                this.Bind(ViewModel,
+                    vm => vm.SpinnerVisibility,
+                    v => v.Spinner.Visibility)
+                .DisposeWith(disposables);
+
+                this.BindCommand(ViewModel,
+                    vm => vm.FireButtonCommand,
+                    v => v.BtnSave)
+                .DisposeWith(disposables);
+
+                this.ViewModel.ButtonInteraction.RegisterHandler(interaction =>
+                {
+                    //Observable.Start(() => { })
+                    //.Delay(TimeSpan.FromSeconds(3))
+                    //.Subscribe(_ =>
+                    //{
+                    //    MessageBox.Show($"Message 1 {interaction.Input.Name}", "Title 1", MessageBoxButton.OK);
+                    //});
+
+                    MessageBox.Show($"Message 1 {interaction.Input.Name}", "Title 1", MessageBoxButton.OK);
+
+                    interaction.SetOutput(interaction.Input.Name);
+                });
+            });
+        }
+    }
+}
